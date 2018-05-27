f6670791a6017fda64a3decb4b08e24e41fda73a
Priefy <peachy-lovly-mom@hotmail.com>
2018-05-27 11:22:58 +0700

    add another test

--- src/lab2/utilities.test.js
+++ src/lab2/utilities.test.js
@@ -4,3 +4,8 @@ it('Should not return 5 when input are 1 and 3', () => {
     const result = util.sum(1, 3)
     expect(result).not.toEqual(5);
 })
+
+it('Should return array of 3 when the input is "yes"', () => {
+    const arr = util.getArrayOf3Items('yes')
+    expect(arr).toHaveLength(3);
+})
